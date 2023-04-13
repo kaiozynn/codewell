19897ca11c67518a09d4d3008d7758c6e7463228
Caio Ferreira <116135148+kaiozynn@users.noreply.github.com>
2023-04-13 18:14:13 -0300

    Atualizar o script.js
    mudei o display do botão de menu

--- ondeck/script.js
+++ ondeck/script.js
@@ -4,9 +4,11 @@ var menu = document.querySelector('.menu')
 
 botao.addEventListener("click", () => {
     menu.style.display = 'block'
+    botao.style.display = "none"
 })
 
 closeMenu.addEventListener('click', () => {
     menu.style.display = 'none'
+    botao.style.display = "block"
 })
 
